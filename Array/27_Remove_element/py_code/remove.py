274841fe4101b6a2066c0d5f758546cd87050e34
Nhung-Ubuntu-Home <home@email.com>
2021-04-06 18:15:17 +0200

    27. Python code shorten

--- Array/27_Remove_element/py_code/remove.py
+++ Array/27_Remove_element/py_code/remove.py
@@ -1,16 +1,7 @@
 def removeElement(nums, val):
-    i = 0
-    count = 0
-    while (i < len(nums)):
-        if (nums[i] == val):
-            j = i
-            while (i < len(nums) - 1):
-                if (nums[i + 1] != val):
-                    nums[j] = nums[i + 1]
-                    count += 1
-                    j += 1
-                i += 1
-        else:
-            count += 1
-        i +=1
-    return (nums[0:count])
+    j = 0
+    for i in nums:
+        if i !=val:
+            nums[j] = i
+            j+=1
+    return (nums[0:j])
