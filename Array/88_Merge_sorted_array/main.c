92258aed45b044c9e5de1c3c2b4a140bfd0c2766
Nhung-Ubuntu-Home <home@email.com>
2021-04-06 12:37:52 +0200

    88. Update C code

--- Array/88_Merge_sorted_array/main.c
+++ Array/88_Merge_sorted_array/main.c
@@ -6,51 +6,23 @@ void	merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n)
 	int i;
 	int j;
 	int k;
-	int *tmp;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	if (nums1Size == 0 || nums2Size == 0)
+	if (nums1Size <= 0 || nums2Size <= 0)
 		return ;
-	tmp = (int *)malloc(sizeof(int) * m);
-	if (!tmp)
-		return ;
-	while (i < m)
+	i = m - 1;
+	j = n - 1;
+	k = m + n - 1;
+	while (j >= 0 && i >= 0)
 	{
-		tmp[i] = nums1[i];
-		i++;
+		if (nums1[i] > nums2[j])
+			nums1[k--] = nums1[i--];
+		else
+			nums1[k--] = nums2[j--];
 	}
-	i = 0;
-	while (k < m + n)
-	{
-		if (j < n && i < m)
-		{
-			if (tmp[i] < nums2[j])
-			{
-				nums1[k] = tmp[i];
-				i++;
-			}
-			else
-			{
-				nums1[k] = nums2[j];
-				j++;
-			}
-		}
-		else if (j == n)
-		{
-			nums1[k] = tmp[i];
-			i++;
-		}
-		else if (i == m)
-		{
-			nums1[k] = nums2[j];
-			j++;
-		}
-		k++;
-	}
-	free(tmp);
-	tmp = NULL;
+	while (i >= 0)
+		nums1[k--] = nums1[i--];
+	while (j >= 0)
+		nums1[k--] = nums2[j--];
 }
 
 void	print_array(int *nums, int numsSize)
